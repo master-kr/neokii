fe00deafca27466c25ce9f48a5a0cd79ad5b61e5
master-kr <136075195+master-kr@users.noreply.github.com>
2025-09-20 10:33:10 +0900

    Update events.py

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -743,13 +743,13 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
   },
 
   EventName.doorOpen: {
-    #ET.SOFT_DISABLE: user_soft_disable_alert("Door Open"),
-    #ET.NO_ENTRY: NoEntryAlert("Door Open"),
+    ET.SOFT_DISABLE: user_soft_disable_alert("Door Open"),
+    ET.NO_ENTRY: NoEntryAlert("Door Open"),
   },
 
   EventName.seatbeltNotLatched: {
-    #ET.SOFT_DISABLE: user_soft_disable_alert("Seatbelt Unlatched"),
-    #ET.NO_ENTRY: NoEntryAlert("Seatbelt Unlatched"),
+    ET.SOFT_DISABLE: user_soft_disable_alert("Seatbelt Unlatched"),
+    ET.NO_ENTRY: NoEntryAlert("Seatbelt Unlatched"),
   },
 
   EventName.espDisabled: {
